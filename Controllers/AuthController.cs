4e53ccf5cfd81ad0243f4c5add6d0851917e4b1e
ChanisaFung <chanisafung987@gmail.com>
2026-03-26 16:04:42 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudApp.Data;
+using System.Linq;
+
+namespace CrudApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users
+                .FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudApp.Models;
+using CRUDApp.Models;
+using System.Collections.Generic;
+
+
+namespace CrudApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+
