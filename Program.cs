1703266fae47e7f8c74516d52883c32ecf2d30eb
ChanisaFung <chanisafung987@gmail.com>
2026-03-27 15:27:50 -0400

    Solucione el problema de conexion ala basa de datos

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using CrudApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;   
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
