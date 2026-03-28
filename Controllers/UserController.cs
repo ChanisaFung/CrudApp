92ba8ef9a27e388802149969f1f6dc74b4731bc3
ChanisaFung <chanisafung987@gmail.com>
2026-03-27 20:14:24 -0400

    Recuperar proyecto completo con SQLite e interfaz de usuario

---
+++ Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudApp.Data;
+using CrudApp.Models;
+using System.Linq;
+
+namespace CrudApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // READ
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+
+        // CREATE (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE (POST)
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // EDIT (GET)
+        public IActionResult Edit(int id)
+        {
+            var user = _context.Users.Find(id);
+            return View(user);
+        }
+
+        // EDIT (POST)
+        [HttpPost]
+        public IActionResult Edit(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
